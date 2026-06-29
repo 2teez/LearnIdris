c3d9936f941b5d9dd246eecaa4af06654c99a044
timothy adigun <adigun.timothy@gmail.com>
2026-06-29 10:15:18 +0100

    include several options

--- makeidr.sh
+++ makeidr.sh
@@ -13,6 +13,7 @@ function usage() {
     echo "Available options:"
     echo "------------------"
     echo "-c: compile the Idris file."
+    echo "-d: delete the compiled executable."
     echo "-h: show this help message."
     echo "-o: generate standalone Idris file."
     echo "-r: run the compiled executable."
@@ -23,11 +24,10 @@ function check_file() {
     filename="${1}"
     file_extension="${filename##*.}"
     file_basename="${filename%.*}"
-    [ "${file_extension}" != "idr" ] && { filename="${file_basename}.idr"; }
+    [ "${file_extension}" != "idr" ] && { filename="${file_basename^}.idr"; }
 }
 
-IDRIS="
-module Main
+IDRIS="module Main
 
 main : IO ()
 main = putStrLn \"Hello, World!\"
@@ -37,33 +37,46 @@ if [ "${#}" -ne 2 ]; then
     usage
 fi
 
-optionstring="c:o:r:h"
+optionstring="c:d:o:r:h"
 while getopts "${optionstring}" opt; do
     case $opt in
         c)
             filename="${OPTARG}"
             echo "Compiling $filename..."
-            #idris --output-dir . $filename
+            check_file "${filename}"
+
+            idris2 "${filename}" -o "${filename%.*}"
+            ;;
+
+        d)
+            filename="${OPTARG}"
+            echo "Deleting compiled executable..."
+
+            rm -rf "build"
+            rm -f "${filename}"
             ;;
         o)
             filename="${OPTARG}"
             echo "Generating standalone Idris file..."
             check_file "${filename}"
-            echo "$IDRIS" > "$filename"
+            echo "$IDRIS" > "${filename}"
 
             idris2 "${filename}" -o "${filename%.*}"
-            ./"${filename%.*}"
+            ./build/exec/"${filename%.*}"
             ;;
         r)
-            output_dir="${OPTARG}"
+            filename="${OPTARG}"
             echo "Running compiled executable..."
-            #./$output_dir/Main
+            check_file "${filename}"
+
+            idris2 "${filename}" -o "${filename%.*}"
+            ./build/exec/"${filename%.*}"
             ;;
         h)
             usage
             ;;
         \?)
-            echo "Invalid option: -$OPTARG" >&2
+            echo "Invalid option: -${OPTARG}" >&2
             usage
             ;;
     esac
