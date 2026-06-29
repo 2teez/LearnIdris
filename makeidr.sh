4325a199721e057df0a15721c0d8bafb5f9b18e6
timothy adigun <adigun.timothy@gmail.com>
2026-06-29 09:47:26 +0100

    bash script to idris

---
+++ makeidr.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+# Date: 29/06/2026
+# Description: A script for programming Idris
+# Author: omitida
+#
+
+filename=
+
+function usage() {
+    echo "Usage: $0 <option> <filename>"
+    echo "  "
+
+    echo "Available options:"
+    echo "------------------"
+    echo "-c: compile the Idris file."
+    echo "-h: show this help message."
+    echo "-o: generate standalone Idris file."
+    echo "-r: run the compiled executable."
+    exit 1
+}
+
+function check_file() {
+    filename="${1}"
+    file_extension="${filename##*.}"
+    file_basename="${filename%.*}"
+    [ "${file_extension}" != "idr" ] && { filename="${file_basename}.idr"; }
+}
+
+IDRIS="
+module Main
+
+main : IO ()
+main = putStrLn \"Hello, World!\"
+"
+
+if [ "${#}" -ne 2 ]; then
+    usage
+fi
+
+optionstring="c:o:r:h"
+while getopts "${optionstring}" opt; do
+    case $opt in
+        c)
+            filename="${OPTARG}"
+            echo "Compiling $filename..."
+            #idris --output-dir . $filename
+            ;;
+        o)
+            filename="${OPTARG}"
+            echo "Generating standalone Idris file..."
+            check_file "${filename}"
+            echo "$IDRIS" > "$filename"
+
+            idris2 "${filename}" -o "${filename%.*}"
+            ./"${filename%.*}"
+            ;;
+        r)
+            output_dir="${OPTARG}"
+            echo "Running compiled executable..."
+            #./$output_dir/Main
+            ;;
+        h)
+            usage
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            ;;
+    esac
+done
